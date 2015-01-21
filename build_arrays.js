84408d34be29e925c241c171557923800c3bbf70
Jesse Copeland <jesse.copeland@gmail.com>
2015-01-20 18:28:59 -1000

    arrays filled in

--- build_arrays.js
+++ build_arrays.js
@@ -3,54 +3,75 @@
  **/
 
 // Create an empty array.
+var emptyArr = [];
 
 
 // Create an array of the colors of the rainbow.
-
+var rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // Create an array of gems.
-
+var gems = ['ruby', 'emerald', 'diamond'];
 
 // Create an array of military ranks.
+var militaryRanks = ['Private', 'PFC', 'Specialist', 'Corporal', 'Sergeant'];
 
 
 // Create an array of road signs.
+var roadSigns = ['stop', 'pedestrian crossing', 'speed limit', 'watch for falling rocks'];
 
 
 // Create an array containing components of a tree.
-
+var partsOfTree = ['trunk', 'branches', 'leaves', 'fruit'];
 
 // Create an array of prime numbers less than 20.
+var primeNumsTo20 = [2,3,5,7,11,13,17,19];
+
+// function isPrime (n) {
+//   var sqrtRndUp = Math.ceil(Math.sqrt(n));
+
+//   for (var i = sqrtRndUp; i > 2; i--) {
+//     console.log('n',n,'i',i);
+//     if (n % i === 0) {return false;}
+//   }
+//   return true;
+// }
 
 
 // Create an array of words programmers typically say while programming.
+var coderWords = ['fuck', 'what', 'why', 'dammit'];
 
 
 // Create an array of elements of the periodic table.
-
+var elements = ['Helium', 'Neon', 'Argon', 'Krypton', 'Xenon', 'Radon'];
 
 // Create an array representing the coins currently in your pocket.
-
+var myCoins = [];
 
 // Create an array of items you bring to class daily.
-
+var myStuff = ['backpack', 'iPhone', 'wallet', 'keys', 'headphones'];
 
 // Create an array of web developer role titles.
+var devRoles = ['tech lead', 'sys admin', 'developer', 'QA', 'DBA'];
 
 
 // Create an array of good luck charms.
+var charms = ['rabbits foot', 'four leaf clover', 'horseshoe'];
 
 
 // Create an array of mythical animals.
+var mythicalCreatures = ['unicorn', 'hippogriff', 'dragon', 'centaur'];
 
 
 // Create an array of drinks.
+var drinks = ['Coke', 'Dr Pepper', 'Pepsi', 'Orange Crush'];
 
 
 // Create an array of emoticons.
+var emoticons = [";-)", "(=O=)", "<=o=>", ":-/"];
 
 
 // Create an array with the letters of the word 'array'.
+var arrayLetters = 'array'.split('');
 
 
 
